c8feb8fc5132e48097666394a832194baf03322d
juzwaista <velayo.joshua@ncst.edu.ph>
2026-09-12 08:30:48 +0800

    Fix URL encoding bug in staff invite email template

--- resources/views/emails/staff/invite.blade.php
+++ resources/views/emails/staff/invite.blade.php
@@ -7,13 +7,13 @@ You have been invited to join the shop team as a staff member.
 
 Please click the button below to set up your account and choose your username and password.
 
-<x-mail::button :url="url('/staff/setup?token='.$token.'&email='.$email)">
+<x-mail::button :url="url('/staff/setup?token='.$token.'&email='.urlencode($email))">
 Set Up Account
 </x-mail::button>
 
 If you're having trouble clicking the "Set Up Account" button, copy and paste the URL below into your web browser:
 
-<span class="break-all">[{{ url('/staff/setup?token='.$token.'&email='.$email) }}]({{ url('/staff/setup?token='.$token.'&email='.$email) }})</span>
+<span class="break-all">[{{ url('/staff/setup?token='.$token.'&email='.urlencode($email)) }}]({{ url('/staff/setup?token='.$token.'&email='.urlencode($email)) }})</span>
 
 Thanks,<br>
 {{ config('app.name') }}
